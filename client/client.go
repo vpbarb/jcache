c0e0881683d06327b9b1e7bd40d99cc92b9f7fa1
Vadim Petrov <vadim.petrov@lazada.com>
2016-08-20 15:07:40 +0300

    Add all operations to client

--- client/client.go
+++ client/client.go
@@ -1,10 +1,10 @@
 package client
 
 import (
-	"fmt"
-	//"log"
 	"errors"
+	"fmt"
 	"net"
+	"strconv"
 	"time"
 
 	"gopkg.in/fatih/pool.v2"
@@ -47,38 +47,117 @@ func New(addr, user, password string, timeout time.Duration, maxConnections int)
 
 // Keys returns all keys
 func (c *Client) Keys() (keys []string, err error) {
-	err = c.call(newKeysResponseFormatter(&keys), 0, "KEYS")
+	err = c.call(newKeysResponseFormatter(&keys), "KEYS")
 	return
 }
 
 // TTL returns ttl of key
 func (c *Client) TTL(key string) (ttl time.Duration, err error) {
-	err = c.call(newTTLResponseFormatter(&ttl), 1, "TTL %s", key)
+	err = c.call(newTTLResponseFormatter(&ttl), "TTL %s", key)
 	return
 }
 
 // Get returns value by key
 func (c *Client) Get(key string) (value string, err error) {
-	err = c.call(newValueResponseFormatter(&value), 1, "GET %s", key)
+	err = c.call(newValueResponseFormatter(&value), "GET %s", key)
 	return
 }
 
 // Set sets new key value
 func (c *Client) Set(key, value string, ttl time.Duration) error {
-	return c.call(nilResponseFormatter, 0, `SET %s "%s" %s`, key, value, ttl)
+	return c.call(nilResponseFormatter, `SET %s "%s" %s`, key, value, ttl)
 }
 
 // Update updates existing key
 func (c *Client) Update(key, value string) error {
-	return c.call(nilResponseFormatter, 0, `UPD %s "%s"`, key, value)
+	return c.call(nilResponseFormatter, `UPD %s "%s"`, key, value)
 }
 
 // Delete deletes value by key
 func (c *Client) Delete(key string) error {
-	return c.call(nilResponseFormatter, 0, "DEL %s", key)
+	return c.call(nilResponseFormatter, "DEL %s", key)
 }
 
-func (c *Client) call(responseFormatter responseFormatFunc, minLines int, command string, params ...interface{}) error {
+// HashCreate creates new hash with ttl
+func (c *Client) HashCreate(key string, ttl time.Duration) error {
+	return c.call(nilResponseFormatter, "HCREATE %s %s", key, ttl)
+}
+
+// HashSet add new field to hash
+func (c *Client) HashSet(key, field, value string) error {
+	return c.call(nilResponseFormatter, `HSET %s %s "%s"`, key, field, value)
+}
+
+// HashGet returns hash field value
+func (c *Client) HashGet(key, field string) (value string, err error) {
+	err = c.call(newValueResponseFormatter(&value), "HGET %s %s", key, field)
+	return
+}
+
+// HashGetAll returns all hash fields values
+func (c *Client) HashGetAll(key string) (hash map[string]string, err error) {
+	hash = make(map[string]string)
+	err = c.call(newHashResponseFormatter(hash), "HGETALL %s", key)
+	return
+}
+
+// HashDelete deletes field from hash
+func (c *Client) HashDelete(key, field string) error {
+	return c.call(nilResponseFormatter, `HDEL %s %s`, key, field)
+}
+
+// HashKeys returns all hash fields
+func (c *Client) HashKeys(key string) (keys []string, err error) {
+	err = c.call(newKeysResponseFormatter(&keys), "HKEYS %s", key)
+	return
+}
+
+// HashLength returns count of hash elements
+func (c *Client) HashLength(key string) (len int, err error) {
+	err = c.call(newLenResponseFormatter(&len), "HLEN %s", key)
+	return
+}
+
+// ListCreate creates new list with ttl
+func (c *Client) ListCreate(key string, ttl time.Duration) error {
+	return c.call(nilResponseFormatter, "LCREATE %s %s", key, ttl)
+}
+
+// ListRightPush adds new value to the list ending
+func (c *Client) ListRightPush(key, value string) error {
+	return c.call(nilResponseFormatter, `LRPUSH %s "%s"`, key, value)
+}
+
+// ListLeftPush adds new value to the list beginning
+func (c *Client) ListLeftPush(key, value string) error {
+	return c.call(nilResponseFormatter, `LLPUSH %s "%s"`, key, value)
+}
+
+// ListRightPop returns and removes the value from the list ending
+func (c *Client) ListRightPop(key string) (value string, err error) {
+	err = c.call(newValueResponseFormatter(&value), "LRPOP %s", key)
+	return
+}
+
+// ListLeftPop returns and removes the value from the list beginning
+func (c *Client) ListLeftPop(key string) (value string, err error) {
+	err = c.call(newValueResponseFormatter(&value), "LLPOP %s", key)
+	return
+}
+
+// ListLength returns count of list elements
+func (c *Client) ListLength(key string) (len int, err error) {
+	err = c.call(newLenResponseFormatter(&len), "LLEN %s", key)
+	return
+}
+
+// ListRange returns all list values from start to stop
+func (c *Client) ListRange(key string, start, stop int) (values []string, err error) {
+	err = c.call(newValuesResponseFormatter(&values), "LRANGE %s %d %d", key, start, stop)
+	return
+}
+
+func (c *Client) call(responseFormatter responseFormatFunc, command string, params ...interface{}) error {
 	conn, err := c.connPool.Get()
 	if err != nil {
 		return err
@@ -88,9 +167,6 @@ func (c *Client) call(responseFormatter responseFormatFunc, minLines int, comman
 	response, err := call(conn, conn, fmt.Sprintf(command, params...))
 	if err != nil {
 		return err
-	}
-	if len(response) < minLines {
-		return errors.New("Invalid response rows count")
 	}
 
 	return responseFormatter(response)
@@ -105,14 +181,56 @@ func newKeysResponseFormatter(keys *[]string) responseFormatFunc {
 
 func newValueResponseFormatter(value *string) responseFormatFunc {
 	return func(response []string) (err error) {
+		if len(response) < 1 {
+			return errors.New("Invalid response rows count")
+		}
 		*value, err = parseValue(response[0])
+		return
+	}
+}
+
+func newValuesResponseFormatter(values *[]string) responseFormatFunc {
+	return func(response []string) (err error) {
+		for _, line := range response {
+			v, err := parseValue(line)
+			if err != nil {
+				return err
+			}
+			*values = append(*values, v)
+		}
 		return
 	}
 }
 
 func newTTLResponseFormatter(ttl *time.Duration) responseFormatFunc {
 	return func(response []string) (err error) {
+		if len(response) < 1 {
+			return errors.New("Invalid response rows count")
+		}
 		*ttl, err = parseTTL(response[0])
+		return
+	}
+}
+
+func newHashResponseFormatter(hash map[string]string) responseFormatFunc {
+	return func(response []string) (err error) {
+		for _, line := range response {
+			field, value, err := parseHashField(line)
+			if err != nil {
+				return err
+			}
+			hash[field] = value
+		}
+		return
+	}
+}
+
+func newLenResponseFormatter(length *int) responseFormatFunc {
+	return func(response []string) (err error) {
+		if len(response) < 1 {
+			return errors.New("Invalid response rows count")
+		}
+		*length, err = strconv.Atoi(response[0])
 		return
 	}
 }
--- client/example/main.go
+++ client/example/main.go
@@ -14,14 +14,24 @@ func main() {
 		log.Fatalf("Client creation error: %s", err)
 	}
 
+	testValueType(client)
+	testHashType(client)
+	testListType(client)
+}
+
+func testValueType(client *client.Client) {
+	log.Print("Value type operations")
+
+	key := "key1"
+
 	keys, err := client.Keys()
 	log.Printf("Keys: %v", keys)
 	if err != nil {
 		log.Printf("Error: %s", err)
 	}
 
-	err = client.Set("key1", "value1", time.Hour)
-	log.Printf("Set: %s = %s", "key1", "value1")
+	err = client.Set(key, "value1", time.Hour)
+	log.Printf("Set: %s", key)
 	if err != nil {
 		log.Printf("Error: %s", err)
 	}
@@ -32,32 +42,64 @@ func main() {
 		log.Printf("Error: %s", err)
 	}
 
-	value, err := client.Get("key1")
-	log.Printf("Get: %s = %s", "key1", value)
+	value, err := client.Get(key)
+	log.Printf("Get: %s = %s", key, value)
 	if err != nil {
 		log.Printf("Error: %s", err)
 	}
 
-	ttl, err := client.TTL("key1")
-	log.Printf("TTL: %s = %s", "key1", ttl)
+	ttl, err := client.TTL(key)
+	log.Printf("TTL: %s = %s", key, ttl)
 	if err != nil {
 		log.Printf("Error: %s", err)
 	}
 
-	err = client.Update("key1", "new_value1")
-	log.Printf("Set: %s = %s", "key1", "new_value1")
+	err = client.Update(key, "new_value1")
+	log.Printf("Set: %s = %s", key, "new_value1")
 	if err != nil {
 		log.Printf("Error: %s", err)
 	}
 
-	value, err = client.Get("key1")
-	log.Printf("Get: %s = %s", "key1", value)
+	value, err = client.Get(key)
+	log.Printf("Get: %s = %s", key, value)
 	if err != nil {
 		log.Printf("Error: %s", err)
 	}
 
-	err = client.Delete("key1")
-	log.Printf("Delete: %s", "key1")
+	err = client.Delete(key)
+	log.Printf("Delete: %s", key)
+	if err != nil {
+		log.Printf("Error: %s", err)
+	}
+
+	value, err = client.Get(key)
+	log.Printf("Get: %s = %s", key, value)
+	if err != nil {
+		log.Printf("Error: %s", err)
+	}
+
+	keys, err = client.Keys()
+	log.Printf("Keys: %v", keys)
+	if err != nil {
+		log.Printf("Error: %s", err)
+	}
+}
+
+func testHashType(client *client.Client) {
+	log.Print("Hash type operations")
+
+	key := "hash1"
+	field1 := "field1"
+	field2 := "field2"
+
+	keys, err := client.Keys()
+	log.Printf("Keys: %v", keys)
+	if err != nil {
+		log.Printf("Error: %s", err)
+	}
+
+	err = client.HashCreate(key, time.Hour)
+	log.Printf("Create: %s", key)
 	if err != nil {
 		log.Printf("Error: %s", err)
 	}
@@ -68,4 +110,153 @@ func main() {
 		log.Printf("Error: %s", err)
 	}
 
+	ttl, err := client.TTL(key)
+	log.Printf("TTL: %s = %s", key, ttl)
+	if err != nil {
+		log.Printf("Error: %s", err)
+	}
+
+	err = client.HashSet(key, field1, "value1")
+	log.Printf("Hash set: %s[%s]", key, field1)
+	if err != nil {
+		log.Printf("Error: %s", err)
+	}
+
+	value, err := client.HashGet(key, field1)
+	log.Printf("Hash get: %s[%s] = %s", key, field1, value)
+	if err != nil {
+		log.Printf("Error: %s", err)
+	}
+
+	err = client.HashSet(key, field2, "value2")
+	log.Printf("Hash set: %s[%s]", key, field2)
+	if err != nil {
+		log.Printf("Error: %s", err)
+	}
+
+	values, err := client.HashGetAll(key)
+	log.Printf("Hash get all: %s = %+v", key, values)
+	if err != nil {
+		log.Printf("Error: %s", err)
+	}
+
+	keys, err = client.HashKeys(key)
+	log.Printf("Hash keys: %s = %+v", key, keys)
+	if err != nil {
+		log.Printf("Error: %s", err)
+	}
+
+	len, err := client.HashLength(key)
+	log.Printf("Hash length: %s = %d", key, len)
+	if err != nil {
+		log.Printf("Error: %s", err)
+	}
+
+	err = client.HashDelete(key, field1)
+	log.Printf("Hash delete: %s[%s]", key, field1)
+	if err != nil {
+		log.Printf("Error: %s", err)
+	}
+
+	value, err = client.HashGet(key, field1)
+	log.Printf("Hash get: %s[%s] = %s", key, field1, value)
+	if err != nil {
+		log.Printf("Error: %s", err)
+	}
+
+	err = client.Delete(key)
+	log.Printf("Delete: %s", key)
+	if err != nil {
+		log.Printf("Error: %s", err)
+	}
+
+	keys, err = client.Keys()
+	log.Printf("Keys: %v", keys)
+	if err != nil {
+		log.Printf("Error: %s", err)
+	}
+}
+
+func testListType(client *client.Client) {
+	log.Print("List type operations")
+
+	key := "list1"
+
+	keys, err := client.Keys()
+	log.Printf("Keys: %v", keys)
+	if err != nil {
+		log.Printf("Error: %s", err)
+	}
+
+	err = client.ListCreate(key, time.Hour)
+	log.Printf("Create: %s", key)
+	if err != nil {
+		log.Printf("Error: %s", err)
+	}
+
+	keys, err = client.Keys()
+	log.Printf("Keys: %v", keys)
+	if err != nil {
+		log.Printf("Error: %s", err)
+	}
+
+	ttl, err := client.TTL(key)
+	log.Printf("TTL: %s = %s", key, ttl)
+	if err != nil {
+		log.Printf("Error: %s", err)
+	}
+
+	err = client.ListRightPush(key, "value1")
+	log.Printf("List right push: %s", key)
+	if err != nil {
+		log.Printf("Error: %s", err)
+	}
+
+	err = client.ListLeftPush(key, "value2")
+	log.Printf("List left push: %s", key)
+	if err != nil {
+		log.Printf("Error: %s", err)
+	}
+
+	err = client.ListRightPush(key, "value3")
+	log.Printf("List right push: %s", key)
+	if err != nil {
+		log.Printf("Error: %s", err)
+	}
+
+	values, err := client.ListRange(key, 0, 2)
+	log.Printf("List range: %s = %+v", key, values)
+	if err != nil {
+		log.Printf("Error: %s", err)
+	}
+
+	len, err := client.ListLength(key)
+	log.Printf("List length: %s = %d", key, len)
+	if err != nil {
+		log.Printf("Error: %s", err)
+	}
+
+	value, err := client.ListRightPop(key)
+	log.Printf("List right pop: %s = %s", key, value)
+	if err != nil {
+		log.Printf("Error: %s", err)
+	}
+
+	value, err = client.ListLeftPop(key)
+	log.Printf("List right pop: %s = %s", key, value)
+	if err != nil {
+		log.Printf("Error: %s", err)
+	}
+
+	err = client.Delete(key)
+	log.Printf("Delete: %s", key)
+	if err != nil {
+		log.Printf("Error: %s", err)
+	}
+
+	keys, err = client.Keys()
+	log.Printf("Keys: %v", keys)
+	if err != nil {
+		log.Printf("Error: %s", err)
+	}
 }
